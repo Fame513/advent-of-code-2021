4b4c0f397a19f723c5f391cea2f7f9cda6cbbf04
Yaroslav <yaroslavnekryach@gmail.com>
2021-12-01 22:10:28 +0200

    Prepare Day 2

---
+++ day2.ts
@@ -0,0 +1,57 @@
+import {getInput, getTestFunction, sendAnswer} from './helper';
+
+const DAY = 2;
+
+tests();
+run().then(async ([result1, result2]) => {
+  console.log('Part 1:', result1);
+  // await sendAnswer(DAY, 1, result1.toString());
+  console.log('Part 2:', result2);
+  // await sendAnswer(DAY, 2, result2.toString());
+
+});
+
+function calculatePart1(input: number[]): number {
+  let result = 0
+  for(let i = 1; i < input.length; i++) {
+    if (input[i] > input[i-1]) {
+      result++;
+    }
+  }
+  return result;
+}
+
+function calculatePart2(input: number[]): number {
+  let result = 0
+  for(let i = 3; i < input.length; i++) {
+    if (input[i] > input[i-3]) {
+      result++;
+    }
+  }
+  return result;
+}
+
+
+function parse(input: string): number[] {
+  return input.split('\n')
+    .map(row => +row)
+}
+
+export async function run() {
+  const input: string = await getInput(DAY);
+  const result1 = calculatePart1(parse(input));
+  const result2 = calculatePart2(parse(input));
+  return [result1, result2]
+}
+
+function tests() {
+  const part1Test = getTestFunction((input) => calculatePart1(input));
+  const part2Test = getTestFunction((input) => calculatePart2(input));
+  part1Test([199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 7);
+
+  console.log('---------------------');
+
+  part2Test([199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 5);
+
+  console.log('---------------------');
+}
